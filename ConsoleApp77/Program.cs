59067dfe431711495eb8ea2754013128a88b817f
ygursu <yasemingrs@gmail.com>
2024-02-20 01:49:33 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp77/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavyedengirilensayipozitifnegatif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Lütfen bir sayı giriniz");
+            int sayi = Int32.Parse(Console.ReadLine());
+
+            if (sayi > 0)
+            {
+                Console.WriteLine("Sayı Pozitif");
+            }
+            else
+            {
+                Console.WriteLine("Sayı Negatif");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
